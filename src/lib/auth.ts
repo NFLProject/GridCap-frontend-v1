fe4863238ae06a4566c666171bab0d684109d308
NFLProject <marctaylor723@gmail.com>
2025-10-13 09:36:09 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,12 @@
-export function getToken(): string | null {
+export function getToken() {
   if (typeof window === "undefined") return null;
-  return window.localStorage.getItem("uid");
+  return localStorage.getItem("token");
+}
+export function setToken(token: string) {
+  if (typeof window === "undefined") return;
+  localStorage.setItem("token", token);
+}
+export function clearToken() {
+  if (typeof window === "undefined") return;
+  localStorage.removeItem("token");
 }
