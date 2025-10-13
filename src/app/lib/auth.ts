9ee5fe21287535d64e086f6b4c0e730cc415a01b
NFLProject <marctaylor723@gmail.com>
2025-10-13 09:21:06 +0100

    Delete src/app/lib directory

--- src/app/lib/auth.ts
+++
@@ -1,5 +0,0 @@
-// src/lib/auth.ts
-export function getToken(): string | null {
-  if (typeof window === "undefined") return null;
-  return window.localStorage.getItem("uid");
-}
--- src/app/lib/types.ts
+++
@@ -1,3 +0,0 @@
-// src/lib/types.ts
-export type Me = {export type Me = { id: number; name: string; email: string };
-
